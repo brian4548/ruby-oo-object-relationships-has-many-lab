b25e18f7048b77ecd87bdc49a0b77fddb2927001
brian4548 <brianr4548@gmail.com>
2020-11-24 02:22:33 -0600

    commit tree 010a328aa9a871aba86e7691dcc4a647e8ccea26

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+
+class Artist
+
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Song 
+
+    @@all = []
+
+
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+end 
+
+
